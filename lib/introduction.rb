e5828932d871e30625766283a7625c33bd9d9af1
Jashema Panapa <jcpanapa@gmail.com>
2020-03-24 14:35:37 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I'm learning to program in #{Ruby}."
+  puts "Hi, my name is #{name} and I'm learning to program in #{language}."
 end
