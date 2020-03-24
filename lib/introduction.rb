9b489d432cf219342290b9f6f1f03a98c810f346
Jashema Panapa <jcpanapa@gmail.com>
2020-03-24 14:34:50 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
-def introduction_with_language(Dan, Ember.js)
-  puts "Hi, my name is #{name}."
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I'm learning to program in #{Ruby}."
 end
